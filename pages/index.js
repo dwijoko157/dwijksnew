e945e86f3086394298df546121f62d5615233d27
dwijoko157 <51867326+dwijoko157@users.noreply.github.com>
2022-10-17 12:05:39 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,88 +4,21 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <title>.::[+] Happy Birthday Elennia [+]::.</title>
-<body onload="type_text()" alink="#000000" bgcolor="#000000" vlink="#000000" link="#c0c0c0" text="#000000">
-<script> 
-     alert("Gatau ini apaan haha"); 
-</script>
-<script> 
-     alert("Mau bilang HBD aja"); 
-</script>
-<script> 
-     alert("Yaudah gitu ae hehe"); 
-</script>
-<script> 
-     alert("Makasih\nMaaf kalo ganggu hehe"); 
-</script>
-<div align="center">
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-<script language="Javascript"><!--
-var tl=new Array(
+      <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+      </main>
 
-"   ",
-"   Halo ",
-"   ",
-"   Audionya gabisa autoplay, jadi pencet sendira yaa",
-"   Gatau mau ngetik apaan intinya HBD aja wkwk",
-"   Gatau juga tgl brp ultahnya dulu pas tanya katanya kepo gitu",
-"   Taunya cuma April doang",
-"   Ini udah tgl 30 tgl terakhir kalo udah telat yamaap hehe",
-"   ",
-"   Udah sih gitu ae haha",
-"   Semangat kerjane, kesehatane dijaga kalo cape istirahat",
-"   Eh kmren mau cerita tapi gajadi lupa ini liat ae di bit.ly/3Ksqog4",
-"   "
-
-);
-var speed=10;
-var index=0; text_pos=0;
-var str_length=tl[0].length;
-var contents, row;
-
-function type_text()
-{
-
-  contents='';
-  row=Math.max(0,index-15);
-  while(row<index)
-    contents += tl[row++] + '\r\n';
-  document.forms[0].elements[0].value = contents + tl[index].substring(0,text_pos) + "|";
-  if(text_pos++==str_length)
-  {
-    text_pos=0;
-    index++;
-    if(index!=tl.length)
-    {
-      str_length=tl[index].length;
-      setTimeout("type_text()",500);
-    }
-  } else
-    setTimeout("type_text()",speed);
-}
-//--></script>
-<style type="text/css">BODY {
-	SCROLLBAR-FACE-COLOR: #000000; SCROLLBAR-HIGHLIGHT-COLOR: #000000; SCROLLBAR-SHADOW-COLOR: #000000; SCROLLBAR-BASE-COLOR: #000000
-}
-</style>
-
-<style fprolloverstyle="">
-TEXTAREA {
-	BORDER-LEFT-COLOR: #000000; BACKGROUND: #000000; BORDER-BOTTOM-COLOR: #000000; FONT: 12px Verdana, Verdana, Verdana, Verdana; COLOR: #d3d3d3; BORDER-TOP-COLOR: #000000; BORDER-RIGHT-COLOR: #000000
-}
-</style>
-
-<h2><font color="#cecece" face="Caurier" size="12">Happy Birthday</font></h2>
-<form><font color="#ff0000">
-	<textarea rows="15" cols="90" readonly="readonly"></textarea> 
-</font></form>
-<br><font color="grey" size=1 face="Verdana, Arial, Helvetica, sans-serif"><dwijks21></font> 
-<audio controls autoplay>
-     <source src="https://beatdreamer.com/content/mp3/Happy Birthday Song in English Chipmunk.mp3" type="audio/mpeg">
-                unsupported !! 
-    </audio>
-</div>
-</body>< dwijks21 >
+      <Footer />
+    </div>
 
   )
 }
