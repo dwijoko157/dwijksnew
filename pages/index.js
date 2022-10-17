29a11dd20e6a735b1da2d83daa2dfd10ac209c15
dwijoko157 <51867326+dwijoko157@users.noreply.github.com>
2022-10-17 12:07:17 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>APAANTUH HEHE</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="HALO HEHE" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Gatau mau ngetik apaan hehe, mau bilang makasih ae sih udah 3 bulan <code>pages/index.js</code>
         </p>
       </main>
 
