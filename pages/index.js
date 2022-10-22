b3dc4923fae5c147ae97a2557ca3eea73f8eda66
dwijoko157 <51867326+dwijoko157@users.noreply.github.com>
2022-10-22 15:32:15 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Hitam" />
         <p className="description">
-          Mau cari apaan? Web ini kosong :)
+          Mau cari apaan? Web ini kosong (:
         </p>
       </main>
 
