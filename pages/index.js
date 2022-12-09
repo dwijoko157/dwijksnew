9131adc0d6ad6dcb111f9dc77909f282eca87070
dwijoko157 <51867326+dwijoko157@users.noreply.github.com>
2022-12-09 15:57:39 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,20 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import turtle
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Dwijks21</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+s = turtle.Screen()
+t = turtle.Turtle()
 
-      <main>
-        <Header title="Hitam" />
-        <p className="description">
-          Ndak penting kah diriku?
-        </p>
-      </main>
+s.bgcolor('#262626')
+t.pencolor('#540d6e')
+t.speed(100)
+col = ('#ee4266', '#ffd23f', '#3bceac', '#0ead69')
 
-      <Footer />
-    </div>
-
-  )
-}
+for n in range(5):
+    for x in range(8):
+        t.speed(x+10)
+        for i in range(2):
+            t.pensize(2)
+            t.circle(80+n*20,90)
+            t.lt(90)
+        t.lt(45)
+    t.pencolor(col[n%4])
+s.exitonclick()
