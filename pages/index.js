724e88625f678851398a4a9b01291e5a8d51be67
dwijoko157 <51867326+dwijoko157@users.noreply.github.com>
2022-12-31 20:56:39 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,24 @@
-import turtle
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
-s = turtle.Screen()
-t = turtle.Turtle()
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>HALLO SAYANG</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-s.bgcolor('#262626')
-t.pencolor('#540d6e')
-t.speed(100)
-col = ('#ee4266', '#ffd23f', '#3bceac', '#0ead69')
+      <main>
+        <Header title="SELAMAT TAHUN BARU 2023" />
+        <p className="description">
+          Semangat terus ya. Maaf ngetiknya disini. Dilihat ya syukur, kalo nggak juga ya gapapa
+        </p>
+      </main>
 
-for n in range(5):
-    for x in range(8):
-        t.speed(x+10)
-        for i in range(2):
-            t.pensize(2)
-            t.circle(80+n*20,90)
-            t.lt(90)
-        t.lt(45)
-    t.pencolor(col[n%4])
-s.exitonclick()
+      <Footer />
+    </div>
+
+  )
+}
